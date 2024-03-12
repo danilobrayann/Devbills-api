35eace9a7e2f30b48ee38cf4d45f30ea95c336ac
danilobrayann <danilobrayann@gmail.com>
2024-03-12 12:28:42 -0300

    feat: criação do pitter

--- readme.md
+++ readme.md
@@ -11,3 +11,11 @@ npm i -D @types/express
 <br>
 npx tsc
 <br>
+npm i -D eslint
+<br>
+npx eslint --init
+<br>
+npm i -D prettier eslint-config-prettier eslint-plugin-prettier
+<br>
+npm i -D eslint-plugin-import-helpers
+<br>
--- src/serve.ts
+++ src/serve.ts
@@ -1,6 +1,6 @@
-import express, {  json } from "express";
+import express, { json } from 'express';
 
 const app = express();
 
 app.use(json());
-app.listen(3333, () => console.log('🚀👩‍🚀 rodando na porta 33333'));
+app.listen(3333, () => console.log('🚀👩‍🚀 rodando na porta 3333'));
